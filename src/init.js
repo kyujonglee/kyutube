869e5dd00cb482c7419fc9a254e5dca259c5ceb2
이규종 <kyujong93@naver.com>
2019-10-25 23:06:57 +0900

    heroku

--- src/init.js
+++ src/init.js
@@ -1,13 +1,15 @@
 import "@babel/polyfill";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
+
+import "./db";
+
+import app from "./app";
+
 dotenv.config();
-
-import './db';
-
-import app from './app';
 
 const PORT = process.env.PORT || 4000;
 
-const handleListen = () => console.log(`✅ starting server on http://localhost:${PORT}`)
+const handleListen = () =>
+  console.log(`✅ starting server on http://localhost:${PORT}`);
 
-app.listen(PORT, handleListen)
+app.listen(PORT, handleListen);
